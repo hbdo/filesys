3c2027a8061c0ea18670259d6b0f9ecaf6c7982c
Burak Dogaroglu <hdogaroglu@ku.edu.tr>
2017-05-24 22:58:39 +0300

    Almost finished

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # filesys
+
+- In this project, I implemented a file system with basic functionalities. 
+
+- For starters, I created a core loop to get input from user, parse it, call corresponding function and print the output. All of that code can be found in main method.
+
+- In the first part, I copied and altered the implementation of the ls function. I added some error handling. The keypoints were that, while traversing, I needed to record the found places of filename1 and filename2. Also, at the end, for correctness purposes, I needed to check only the used blocks of a file to calculate the difference. This part works fine and without errors.
+
+- In the second part, I altered the code of ls function again. After error handlings, I recorded the place of the filename1, read the corresponding block to memory, edit the data and write the result back to where it was. This part also works great and without errors.
+
+- In this part, I based my code on md function. I added a few error handling clauses. While traversing the directory for empty spaces, I also checked the full spaces to find the input file and recorded its place. The rest of the code works as expected. I changed the part where a directory is created to file creation. That worked fine as it produced new files as expected. Yet, for copying part, for some reason that I couldn't find, my code does not copy the neccessary parts to the newly created file, so in the end I get mixed results. For instance, a copied file called "new" is produced correctly when I checked with compare method, yet other test cases failed. So, this part has some bugs but implemented almost fully working.
+
--- main.c
+++ main.c
@@ -23,7 +23,7 @@ typedef struct _func_t {
     int argcount;
 } func_t;
 
-func_t methods[8];
+func_t methods[METHOD_COUNT];
 
 int main(int* argc, char** argv){
     mountFS304();
@@ -133,10 +133,26 @@ void compare(char* fname1, char* fname2){
         return;
     }
 
-    diff += countDiff(stoi(_inode_table[fname1_inode].XX,2), stoi(_inode_table[fname2_inode].XX,2));
-    diff += countDiff(stoi(_inode_table[fname1_inode].YY,2), stoi(_inode_table[fname2_inode].YY,2));
-    diff += countDiff(stoi(_inode_table[fname1_inode].ZZ,2), stoi(_inode_table[fname2_inode].ZZ,2));
+    int block1 = 0;
+    int block2 = 0;
+    block1 = stoi(_inode_table[fname1_inode].XX,2);
+    block2 = stoi(_inode_table[fname2_inode].XX,2);
+    if(block1 != 0 && block2 != 0){
+        diff += countDiff(block1, block2);
+    }
 
+    block1 = stoi(_inode_table[fname1_inode].YY,2);
+    block2 = stoi(_inode_table[fname2_inode].YY,2);
+    if(block1 != 0 && block2 != 0){
+        diff += countDiff(block1, block2);
+    }
+
+    block1 = stoi(_inode_table[fname1_inode].ZZ,2);
+    block2 = stoi(_inode_table[fname2_inode].ZZ,2);
+    if(block1 != 0 && block2 != 0){
+        diff += countDiff(block1, block2);
+    }
+    
     printf("The files have %d differences in total\n", diff);
 
 }
@@ -166,6 +182,11 @@ void rname(char* oldname, char* newname){
 		printf("Fatal Error! Aborting.\n");
 		exit(1);
 	}
+
+    if(strlen(newname) == 0){
+        printf("New name must be provided\n");
+        return;
+    }
 
 	// lets traverse the directory entries in all three blocks
 	for (i=0; i<3; i++) {
@@ -211,7 +232,175 @@ void rname(char* oldname, char* newname){
 }
 
 void copy(char* fname1, char* fname2){
+    char itype;
+	int blocks[3];
+	_directory_entry _directory_entries[4];
 
+	int i,j;
+
+	int empty_dblock=-1,empty_dentry=-1;
+	int empty_ientry;
+
+	// non-empty name
+	if (strlen(fname2)==0) {
+		printf("New name must be provided\n");
+		return;
+	}
+
+    if(strcmp(fname1, fname2) == 0){
+        printf("New file name must be different from the original file\n");
+        return;
+    }
+
+	// do we have free inodes
+	if (free_inode_entries == 0) {
+		printf("Error: Inode table is full.\n");
+		return;
+	}
+
+	// read inode entry for current directory
+	// in FS304, an inode can point to three blocks at the most
+	itype = _inode_table[CD_INODE_ENTRY].TT[0];
+	blocks[0] = stoi(_inode_table[CD_INODE_ENTRY].XX,2);
+	blocks[1] = stoi(_inode_table[CD_INODE_ENTRY].YY,2);
+	blocks[2] = stoi(_inode_table[CD_INODE_ENTRY].ZZ,2);
+
+    int fname1_blockIdx = -1;
+    int fname1_dirIdx = -1;
+    int fname1_inode = -1;
+    int fname1_blocks[3];
+
+	// its a directory; so the following should never happen
+	if (itype=='F') {
+		printf("Fatal Error! Aborting.\n");
+		exit(1);
+	}
+
+	// now lets try to see if the name already exists
+	for (i=0; i<3; i++) {
+		if (blocks[i]==0) { 	// 0 means pointing at nothing
+			if (empty_dblock==-1) empty_dblock=i; // we can later add a block if needed
+			continue;
+		}
+
+		readFS304(blocks[i],(char *)_directory_entries); // lets read a directory entry; notice the cast
+
+		// so, we got four possible directory entries now
+		for (j=0; j<4; j++) {
+			if (_directory_entries[j].F=='0') { // means unused entry
+				
+                if (empty_dentry==-1) { empty_dentry=j; empty_dblock=i; } // AAHA! lets keep a note of it, just in case we have to create the new directory
+				continue;
+			} else {
+                if(strcmp(fname1, _directory_entries[j].fname) == 0){
+                    fname1_blockIdx = blocks[i];
+                    fname1_dirIdx = j;
+                    fname1_inode = stoi(_directory_entries[j].MMM, 3);
+                }
+            }
+
+			if (strncmp(fname2,_directory_entries[j].fname, 252) == 0) { // compare with user given name
+					printf("%.252s: Already exists.\n",fname2);
+					return;
+			}
+		}
+
+	}
+
+    if(fname1_blockIdx == -1){
+        printf("Source file not found: %s\n", fname1);
+        return;
+    }
+
+    fname1_blocks[0] = stoi(_inode_table[CD_INODE_ENTRY].XX,2);
+	fname1_blocks[1] = stoi(_inode_table[CD_INODE_ENTRY].YY,2);
+	fname1_blocks[2] = stoi(_inode_table[CD_INODE_ENTRY].ZZ,2);
+
+    int needed_blocks = 0;
+    for(int i=0; i<3; i++){
+        if(fname1_blocks[i] != 0){
+            needed_blocks++;
+        }
+    }
+
+    if(free_disk_blocks < needed_blocks){
+        printf("There is not enough memory to copy file\n");
+        return;
+    }
+
+
+	// so directory name is new
+
+	// if we did not find an empty directory entry and all three blocks are in use; then no new directory can be made
+	if (empty_dentry==-1 && empty_dblock==-1) {
+		printf("Error: Maximum directory entries reached.\n");
+		return;
+	}
+	else { // otherwise
+		if (empty_dentry == -1) { // Great! didn't find an empty entry but not all three blocks have been used
+			empty_dentry=0;
+
+			if ((blocks[empty_dblock] = getBlock())==-1) {  // first get a new block using the block bitmap
+				printf("Error: Disk is full.\n");
+				return;
+			}
+
+			writeFS304(blocks[empty_dblock],NULL);	// write all zeros to the block (there may be junk from the past!)
+
+			switch(empty_dblock) {	// update the inode entry of current dir to reflect that we are using a new block
+				case 0: itos(_inode_table[CD_INODE_ENTRY].XX,blocks[empty_dblock],2); break;
+				case 1: itos(_inode_table[CD_INODE_ENTRY].YY,blocks[empty_dblock],2); break;
+				case 2: itos(_inode_table[CD_INODE_ENTRY].ZZ,blocks[empty_dblock],2); break;
+			}
+		}
+
+
+		// NOTE: all error checkings have already been done at this point!!
+		// time to put everything together
+
+		empty_ientry = getInode();	// get an empty place in the inode table which will store info about blocks for this new directory
+
+		readFS304(blocks[empty_dblock],(char *)_directory_entries);	// read block of current directory where info on this new directory will be written
+		_directory_entries[empty_dentry].F = '1';			// remember we found which directory entry is unused; well, set it to used now
+		strncpy(_directory_entries[empty_dentry].fname,fname2,252);	// put the name in there
+		itos(_directory_entries[empty_dentry].MMM,empty_ientry,3);	// and the index of the inode that will hold info inside this directory
+		writeFS304(blocks[empty_dblock],(char *)_directory_entries);	// now write this block back to the disk
+
+        int new_blocks[needed_blocks];
+        for(int i=0; i<needed_blocks;i++){
+            new_blocks[i] = getBlock();
+        }
+        
+		strncpy(_inode_table[empty_ientry].TT,"FI",2);		// create the inode entry...first, its a directory, so DI
+		strncpy(_inode_table[empty_ientry].XX,"00",2);		// directory is just created; so no blocks assigned to it yet
+		strncpy(_inode_table[empty_ientry].YY,"00",2);
+		strncpy(_inode_table[empty_ientry].ZZ,"00",2);
+
+        char id[2];
+        if(new_blocks[0] != 0){
+            itos(id, new_blocks[0], 2);
+            strncpy(_inode_table[empty_ientry].XX,id,2);		// directory is just created; so no blocks assigned to it yet
+        }
+        if(new_blocks[1] != 0){
+            itos(id, new_blocks[1], 2);
+            strncpy(_inode_table[empty_ientry].YY,id,2);
+        }
+        if(new_blocks[2] != 0){
+            itos(id, new_blocks[2], 2);
+            strncpy(_inode_table[empty_ientry].ZZ,id,2);
+        }
+		writeFS304(BLOCK_INODE_TABLE, (char *)_inode_table);	// phew!! write the inode table back to the disk
+	
+        for(int i=0; i<needed_blocks; i++){
+            if((fname1_blocks[i] != 0) && (new_blocks[i] != 0)){
+                char tempbuff[1024];
+                readFS304(fname1_blocks[i], tempbuff);
+                writeFS304(new_blocks[i], tempbuff);
+            }
+        }
+
+
+    }
 }
 
 void quit(){
